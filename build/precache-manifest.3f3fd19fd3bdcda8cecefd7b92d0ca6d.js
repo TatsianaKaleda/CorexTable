9b2d399164606620b9ffac220c99b80b04edfbf3
home\Nikolai <tatsiana.kaleda.work@gmail.com>
2020-09-10 10:39:04 +0300

    Initial

---
+++ build/precache-manifest.3f3fd19fd3bdcda8cecefd7b92d0ca6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a5c8a1b514ad85dc9c60e83eafe66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0ab898ac7b6169cbc6",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "aefa7ff4b9778f2fb224",
+    "url": "/static/css/main.4727e761.chunk.css"
+  },
+  {
+    "revision": "fb0ab898ac7b6169cbc6",
+    "url": "/static/js/2.213e1316.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.213e1316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefa7ff4b9778f2fb224",
+    "url": "/static/js/main.fb7817aa.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
